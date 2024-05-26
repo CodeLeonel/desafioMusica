0c300c778797357e7829dbfb5e71007d228cacfc
Hugo Leonel <hugoleonel.code@gmail.com>
2024-05-26 17:29:42 -0300

    Inicio do desafio

---
+++ src/main/java/br/com/alura/desafioMusica/DesafioMusicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafioMusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMusicaApplication.class, args);
+	}
+
+}
